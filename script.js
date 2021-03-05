4ef80038b460ee9046a5d9b00c5c689dfdd326c9
Ryan Hutchinson <me@ryanhutchinson.io>
2021-03-04 22:49:32 -0500

    finished syntax for function 2

--- script.js
+++ script.js
@@ -10,24 +10,27 @@ function computerPlay() {
   return result
   }
 
+function playRound(playerSelection, computerSelection) {
+// your code here
+  let playerSelection = lower(prompt("Choose your weapon (Rock, Paper, Scissors): "));
+  let computerSelection = computerPlay() 
+  if (playerSelection = "rock" && computerSelection === "scissors") {
+    return("You WIN! Rock beats Scissors.");
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return("You WIN! Paper beats Rock.");
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return("You WIN! Scissors beats Paper.");
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    return("You LOSE! Paper beats your Rock.");
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return("You LOSE! Scissors beats your Paper.");
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    return("You LOSE! Rock beats your Scissors.");
+  } else (playerSelection === computerSelection)
+    return("It's a TIE!"); 
+}  
 
-function playRound(playerSelection, computerSelection):
-    let playerSelection: upper.(input form ("rock", "paper", "scissors")
-
-    let computerSelection:
-        - calls the function computerPlay 
-        if playerSelection = "rock" && computerSelection === "scissors"
-                return("You WIN! Rock beats Scissors.");
-            else if playerSelection === "paper" && computerSelection === "rock"
-                return("You WIN! Paper beats Rock.");
-            else if PlayerSelection === "scissors" && computerSelection === "paper"
-                return("You WIN! Scissors beats Paper.");
-            else if playerSelection === "rock" && computerSelection === "paper"
-                return("You LOSE! Paper beats your Rock.");
-            else if playerSelection === "paper" && computerSelection === "scissors"
-                return("You LOSE! Scissors beats your Paper.");
-            else if playerSelection === "scissors" && computerSelection === "rock"
-                return("You LOSE! Rock beats your Scissors.");
-            else playerSelection === computerSelection
-                return("It's a TIE!");
-        return results
+// to test function of playRound()
+let playerSelection = 'rock';
+let computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
